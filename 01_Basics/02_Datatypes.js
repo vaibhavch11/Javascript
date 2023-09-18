b0626bff52c5c6abb6ef8cb5a146a67270f6fe1b
vaibhav chauhan <chauhanvaibhav1105@gmail.com>
2023-09-18 13:33:59 +0530

    Data Types

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,49 @@
+
+"use strict"; 
+//Note ,it will treat all JS  code as newer version (it will treat your code on the bases of new standards by js Engine).
+
+
+// alert(3+3)
+// we are using nodejs, not browser.
+
+
+
+console.log(3 
+    +
+    3)  //code readability  should be high.
+
+console.log("Vaibhav");
+
+
+//In earliers days of javascript every browser have there own standard. 
+//so for that ECMA script community/organization has been created for js standards irrespective of browser.
+
+
+
+let name = "vaibhav"
+let age = 18
+let isLogin = true
+
+
+//--------------------------------------premitive DataType------------------------------------------------------
+//number
+//bigInt
+//string
+//boolean
+
+/*
+null => stand alone value (it's a type.)
+for example: let state = null;
+console.log(typeof null) --> object
+
+we will talk about this later** */
+
+
+
+/*
+undefine
+For example: let state; 
+Now that state is undefine.
+console.log(typeof undefine) --> undefine
+
+symbol => unique */
